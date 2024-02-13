52262fd2854f7af874321443f722c1f4924757ff
odutola israel <odutolaisreal11@gmail.com>
2024-02-13 09:23:04 -0800

    made changes

---
+++ donation.js
@@ -0,0 +1,15 @@
+function Dc() {
+    const tog = document.querySelector("#frm")
+    tog.classList.toggle("popUp")
+};
+function ba() {
+    var togg = document.querySelector(".bt")
+    togg.classList.toggle("popUp").style.display = "block"
+};
+function mastercard() {
+    var toggg = document.querySelector(".card2")
+    toggg.classList.toggle("popUp").style.display = "block"
+}; function mastercard() {
+    const to = document.querySelector(".card2")
+    to.classList.toggle("popUp")
+};
