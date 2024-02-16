233a4d94e641f72cb49ad5aaa44e5b55c51513a3
odutola israel <odutolaisreal11@gmail.com>
2024-02-16 10:49:22 -0800

    made changes

--- donation.js
+++ donation.js
@@ -6,10 +6,65 @@ function ba() {
     var togg = document.querySelector(".bt")
     togg.classList.toggle("popUp").style.display = "block"
 };
-function mastercard() {
-    var toggg = document.querySelector(".card2")
-    toggg.classList.toggle("popUp").style.display = "block"
-}; function mastercard() {
-    const to = document.querySelector(".card2")
-    to.classList.toggle("popUp")
-};
+
+const form = document.getElementById("payForm");
+form.addEventListener("submit", payNow);
+
+function pay() {
+    var donation_amt = document.addEventListener("#donation_amt")
+}
+
+
+
+
+
+
+
+
+
+
+
+function pay(e) {
+    e.preventdefault();
+
+    var donation_amt = document.getElementById("donation_amt")
+    FlutterwaveCheckout({
+        public_key: "FLWPUBK_TEST-66274efa295ca60299fd1bdad71a8625-X",
+        tx_ref: txRef,
+        amount: donation_amt,
+        currency: "NGN",
+        redirect_url: "http://127.0.0.1:5501/success.html",
+        meta: {
+            address: address,
+            date: date,
+        },
+
+
+
+
+        customer: {
+            email: email,
+            phone_number: phone,
+            name: name,
+
+
+        },
+        customizations: {
+            title: "UPGC Ministries",
+            description: "Payment for purchase",
+            logo: "http://127.0.0.1:5501/assets/images/UPGC%20LOGO.PNG",
+        },
+        onclose: function () { },
+        callback: function (data) {
+            const reference = data.tx_ref;
+            console.log("This is the data returned after a charge", data);
+            if (data.tx.chargedata == "00" || data.tx.chargedata == "0") {
+                window.location = "http://127.0.0.1:5501/success.html"
+                // redirect to a success page
+            } else {
+                window.location = "http://127.0.0.1:5501/failure.html"
+                // redirect to a failure page.
+            }
+        },
+    });
+}
